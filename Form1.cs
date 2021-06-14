caa872b7a0997f2fb9ed758b63dde53629726cc9
Marco <antnyhern@gmail.com>
2021-06-13 23:12:56 -0500

    Add all project

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TXTWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = { textBox1.Text, textBox2.Text, textBox3.Text };
+
+            using (StreamWriter OutPutFile = new StreamWriter(@"C:\Users\marco\source\repos\TXTWriter\bin\Debug\net5.0-windows\Documents\FileDemo.txt"))
+            {
+                foreach (var itemLines in lines)
+                {
+                    OutPutFile.WriteLine(itemLines);
+                }
+            }
+        }
+    }
+}
